67f51574f644213acff1d26dea22c598707a3f2c
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-22 17:09:28 -0600

    initital spotify nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import SpotifyProvider from "next-auth/providers/Spotify";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { env } from "@env/server.mjs";
+import { prisma } from "@server/db/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,9 +19,10 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    SpotifyProvider({
+      authorization: "https://accounts.spotify.com/authorize?scope=user-read-email,user-top-read",
+      clientId: env.SPOTIFY_CLIENT_ID,
+      clientSecret: env.SPOTIFY_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
