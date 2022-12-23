db59bf8f9ebf3fc504a93574fff84e199a2e0aa4
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-23 01:05:21 -0600

    text adjustment for new font

--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -11,11 +11,13 @@ const Blob: FC = () => {
         height={300}
         className="absolute left-0 right-5 top-40 z-20 mx-auto"
       />
-      <div className="absolute top-[19rem] left-0 right-0 z-20 mx-auto w-52">
-        <h1 className="text-6xl text-black">Imagify</h1>
+      <div className="absolute top-[20rem] left-0 right-5 z-20 mx-auto w-52">
+        <h1 className="text-5xl font-semibold text-black">Imagify</h1>
       </div>
-      <div className="absolute left-0 right-2 top-[18.5rem] z-20 mx-auto w-52">
-        <h1 className="font-outline text-6xl text-transparent">Imagify</h1>
+      <div className="absolute left-0 right-8 top-[19.5rem] z-20 mx-auto w-52 ">
+        <h1 className="font-outline text-5xl font-semibold text-transparent opacity-75">
+          Imagify
+        </h1>
       </div>
     </div>
   );
