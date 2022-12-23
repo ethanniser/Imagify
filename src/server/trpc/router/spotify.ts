a85d27313dbeae125a1f8e5613c078344c13208f
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-23 17:50:09 -0600

    INCOMPLETE, figuring out spotify API

--- src/server/trpc/router/spotify.ts
+++ src/server/trpc/router/spotify.ts
@@ -1,29 +1,30 @@
-import { z } from "zod";
 import { router, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
 export const spotifyRouter = router({
-  hello: protectedProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
-  getAll: protectedProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  getUser: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findUnique({
+      where: { id: ctx.session.user.id },
+      include: { accounts: true },
+    });
   }),
-  getTop: protectedProcedure
-    .input(z.object({ type: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const response = await fetch(
-        `https://api.spotify.com/v1/me/top/${input.type}?limit=20&time_range=long_term`,
-        {
-          headers: {
-            Authorization: `Bearer ${ctx.session /*.token*/}`,
-          },
-        }
-      );
-      const { items } = await response.json();
-      return { res: items };
-    }),
+  // !to fix
+  getTopArtists: protectedProcedure.query(async ({ ctx }) => {
+    const data = ctx.prisma.user.findUnique({
+      where: { id: ctx.session.user.id },
+      include: { accounts: true },
+    });
+    if (!data.accounts) throw new TRPCError({ code: "NOT_FOUND" });
+    const account = data.accounts[0];
+    const response = await fetch(
+      `https://api.spotify.com/v1/me/top/artists?limit=20&time_range=long_term`,
+      {
+        headers: {
+          Authorization: `Bearer ${account.access_token}`,
+        },
+      }
+    );
+    const { items } = await response.json();
+    return { res: items };
+  }),
 });
