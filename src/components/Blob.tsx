4a8691dd2aeecb311e45ef9be27dd40d37fc3ab4
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-23 12:00:30 -0600

    blob to not use absolute to window positioning

--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -3,18 +3,12 @@ import Image from "next/image";
 
 const Blob: FC = () => {
   return (
-    <div>
-      <Image
-        src="/blob.svg"
-        alt="blob"
-        width={300}
-        height={300}
-        className="absolute left-0 right-5 top-40 z-20 mx-auto"
-      />
-      <div className="absolute top-[20rem] left-0 right-5 z-20 mx-auto w-52">
+    <div className="relative -ml-4">
+      <Image src="/blob.svg" alt="blob" width={300} height={300} className="" />
+      <div className="absolute top-40 left-12">
         <h1 className="text-5xl font-semibold text-black">Imagify</h1>
       </div>
-      <div className="absolute left-0 right-8 top-[19.5rem] z-20 mx-auto w-52 ">
+      <div className="absolute top-36 left-10">
         <h1 className="font-outline text-5xl font-semibold text-transparent opacity-75">
           Imagify
         </h1>
