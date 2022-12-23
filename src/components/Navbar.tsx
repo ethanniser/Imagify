7727c2012dd0e5df31bffc76b1b23da71bf0204c
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-22 18:51:27 -0600

    created navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { type FC } from "react";
+import Link from "next/link";
+
+const Navbar: FC = () => {
+  return (
+    <nav className="z-50 bg-neutral-900 py-5">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <div className="mx-auto">
+          <Link href="/">
+            <span className="px-10 text-zinc-300 hover:underline">Home</span>
+          </Link>
+          <Link href="/privacy-policy">
+            <span className="px-10 text-zinc-300 hover:underline">
+              Privacy Policy
+            </span>
+          </Link>
+          <Link href="/about">
+            <span className="px-10 text-zinc-300 hover:underline">About</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
