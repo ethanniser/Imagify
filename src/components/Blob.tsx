92028820421eb9dc323e735fc0896aa06ee68217
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-23 00:49:25 -0600

    adjust blob text

--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -11,10 +11,10 @@ const Blob: FC = () => {
         height={300}
         className="absolute left-0 right-5 top-40 z-20 mx-auto"
       />
-      <div className="absolute top-80 left-0 right-0 z-20 mx-auto w-52">
+      <div className="absolute top-[19rem] left-0 right-0 z-20 mx-auto w-52">
         <h1 className="text-6xl text-black">Imagify</h1>
       </div>
-      <div className="absolute left-0 right-6 top-[19rem] z-20 mx-auto w-52">
+      <div className="absolute left-0 right-2 top-[18.5rem] z-20 mx-auto w-52">
         <h1 className="font-outline text-6xl text-transparent">Imagify</h1>
       </div>
     </div>
