9009f6c730cfc570737185632757957d7d88e004
Ethan Niser <thebestgamer2502@gmail.com>
2022-12-24 13:31:41 -0600

    dleted old commented code

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,37 +3,6 @@ import { trpc } from "../utils/trpc";
 import Image from "next/image";
 
 const Test: FC = () => {
-  // const { data, isLoading } = trpc.spotify.getTopArtists.useQuery();
-  // const names = data?.items.map((item) => (
-  //   <div key={item.name}>{item.name}</div>
-  // ));
-  // const { data: data2, isLoading: isLoading2 } =
-  //   trpc.openai.getGptCompletion.useQuery({ prompt: "what is a tree" });
-  // const { data: data3, isLoading: isLoading3 } =
-  //   trpc.openai.getGptImage.useQuery({ prompt: "a dolphin on a horse" });
-  // console.log(data3);
-  // return (
-  //   <>
-  //     {isLoading ? (
-  //       <div>loading spotify</div>
-  //     ) : names ? (
-  //       <div>{names}</div>
-  //     ) : (
-  //       <div>no spotify data</div>
-  //     )}
-  //     {isLoading2 ? <div>loading openai text</div> : <div>{data2}</div>}
-  //     {isLoading3 ? (
-  //       <Image
-  //         src={data3 ?? "/openAiPlaceholder.png"}
-  //         alt="generated image"
-  //         height={512}
-  //         width={512}
-  //       />
-  //     ) : (
-  //       <div>image loading</div>
-  //     )}
-  //   </>
-  // );
   //*GPT TEXT
   const [GPTout, setGPTout] = useState<string>();
   const [input, setInput] = useState<string>("");
