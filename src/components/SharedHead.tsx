1a2abe853cf493cd48a6d50b3981fc5f6d6ea2b2
Ethan Niser <thebestgamer2502@gmail.com>
2023-01-27 18:02:17 -0600

    preview card and meta tags galore

--- src/components/SharedHead.tsx
+++ src/components/SharedHead.tsx
@@ -4,12 +4,39 @@ import Head from "next/head";
 const SharedHead: FC = () => {
   return (
     <Head>
-      <title>Imagify</title>
+      <title>Imagify- Generate Images Based Off Your Spoitfy</title>
+      <link rel="icon" href="/icon.webp" />
       <meta
         name="description"
-        content="A site that creates images based off your Spotify!"
+        content="Imagify is a web app that generates images based off your Spotify data. 
+        It uses your top artists and tracks to generate a unique image that you can use as a profile picture or wallpaper."
       />
-      <link rel="icon" href="/icon.webp" />
+      <meta
+        name="keywords"
+        content="spotify, spotify profile, spotify image, spotify wallpaper, 
+      spotify profile picture, openai, dalle, chatgpt"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Ethan Niser" />
+
+      <meta name="og:title" content="Imagify" />
+      <meta
+        name="og:description"
+        content="Generate Images Based Off Your Spotify"
+      />
+      <meta name="og:image" content="/previewcard.png" />
+      <meta name="og:url" content="https://imagifymusic.com/" />
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="/previewcard.png" />
+      <meta property="twitter:domain" content="imagifymusic.com" />
+      <meta property="twitter:url" content="https://imagifymusic.com/" />
+      <meta name="twitter:title" content="Imagify" />
+      <meta
+        name="twitter:description"
+        content="Generate Images Based Off Your Spotify"
+      />
+      <meta name="twitter:image" content="" />
     </Head>
   );
 };
