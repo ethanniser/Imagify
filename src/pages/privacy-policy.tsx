54a18cf1cd3d42d5e7e13686ceeb0b92948f8f4d
Ethan Niser <thebestgamer2502@gmail.com>
2023-01-27 20:45:31 -0600

    clarify privacy policy

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -13,19 +13,20 @@ const Home: NextPage = () => {
             <h1 className="p-20 text-6xl text-zinc-100">Privacy Policy</h1>
             <p className="py-5 text-lg text-zinc-100">
               Imagify is a web app powered by the Spotify and OpenAI API{"'"}s.
+              This service is not intended for monetization.
             </p>
             <p className="py-5 text-lg text-zinc-100">
-              By choosing to use this app, you agree to the use of your Spotify
-              account data, and the ability to view your top songs and artists.
+              By choosing to use this app, you agree to the collection and use
+              of your Spotify account data (ID, name and email), and the ability
+              to view your top songs and artists.
             </p>
             <p className="py-5 text-lg text-zinc-100">
               None of the data used by Imagify is shared with any third parties.
               All information is used solely for generating images.
             </p>
             <p className="py-5 text-lg text-zinc-100">
-              Although you can rest assured that your data is not being stored
-              or used maliciously, if you would like to revoke Imagifys
-              permissions,{" "}
+              Although you can rest assured that your data is not being used
+              maliciously, if you would like to revoke Imagifys permissions,{" "}
               <a
                 href={
                   "https://support.spotify.com/us/article/spotify-on-other-apps/"
